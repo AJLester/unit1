f8750c24d3caf37f602a969451ff8948c9d42dc6
AJLester <827451@mystma.org>
2020-09-17 20:08:25 +0000

    top 10 movies

---
+++ unit1/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("central inntalagence");
+        System.out.println("Fast and ferious");
+        System.out.println("merical");
+        System.out.println("the blindside");
+        System.out.println("6 underground");
+        System.out.println("avengers");
+        System.out.println("The other guys");
+        System.out.println("spencer");
+        System.out.println("harry potter");
+        System.out.println("daddys home 2");
+
+    }
+}
