e9ff05b63009c771a7f4336c60858b5bb34cd20d
AJLester <827451@mystma.org>
2020-09-18 20:16:13 +0000

    USA states

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,16 +2,17 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("central inntalagence");
-        System.out.println("Fast and ferious");
-        System.out.println("merical");
-        System.out.println("the blindside");
-        System.out.println("6 underground");
-        System.out.println("avengers");
-        System.out.println("The other guys");
-        System.out.println("spencer");
-        System.out.println("harry potter");
-        System.out.println("daddys home 2");
 
-    }
+
+  boolean livesinAlbertville = false;
+  System.out.println(livesinAlbertville);
+byte howmanystatesarethereintheUSA = 50;
+System.out.println("howmanystatesarethereintheUSA?");
+System.out.println(howmanystatesarethereintheUSA);
+
+}
+
+
+    
+
 }
