14c28919ed73f2fcb4d8c2d22dabc335040bda3a
AJLester <827451@mystma.org>
2020-09-24 20:13:18 +0000

    User Imput

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,14 +1,31 @@
 package app;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
+Scanner input = new Scanner(System.in);
 
-  boolean livesinAlbertville = false;
-  System.out.println(livesinAlbertville);
-byte howmanystatesarethereintheUSA = 50;
-System.out.println("howmanystatesarethereintheUSA?");
-System.out.println(howmanystatesarethereintheUSA);
+System.out.println("Please enter your first name.");
+String name = input.nextLine();
+System.out.println("Hello, " + name);
+
+System.out.println("Please emter your last name.");
+String lastname = input.nextLine();
+System.out.println("Hello, " + lastname);
+
+System.out.println("Please enter your age");
+String age = input.nextLine();
+System.out.println(age);
+
+System.out.println("Please enter your grade");
+String grade = input.nextLine();
+System.out.println(grade);
+
+System.out.println("Please enter your favorite color");
+String color = input.nextLine();
+System.out.println(color);
+
+
 
 }
 
