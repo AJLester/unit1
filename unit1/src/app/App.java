ebb6b9bfb629fc7a48e57caa7b7f50efa34f9308
AJLester <827451@mystma.org>
2020-09-25 20:16:47 +0000

    while loops

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,27 +2,32 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String firstName = "AJ";
-        String lastName = "Lester";
+        
+int count = 0;
+String color = "red";
+boolean colors = true; 
 
-System.out.println(firstName + " " + lastName);
-String person1 = "Bobby";
-  String person2 = "John";
-  String verb1 = "Run";
-  String verb2 = "Eat";
-  String verb3 = "Run";
-  String food = "Pizza";
-  String dayOfWeek = "Monday";
-  String place1 = "Park";
-  String place2 = "Track";
-  String adjective1 = "Delicious";
-  String adjective2 = "Older";
-  System.out.println(person1 + "is a programmer who likes to" + verb1);
-System.out.println("Every");
+while (count <= 10) {
+    System.out.println("The count is " + count);
+    count = count + 1;
+
+}
+System.out.println("count finished");
+while (color == "red");{
+    System.out.println("The color is " + color);
+    color = "green";
 
 
 
 
+
+    
 
     }
+System.out.println("count finished");
+    while (colors == true) {
+    System.out.println("The boolean is " + colors);
+    colors = false; }
+    }
+    
 }
