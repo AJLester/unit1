643943a34063cdd34f040cf606fb139b77a9143f
AJLester <827451@mystma.org>
2020-10-02 20:12:01 +0000

    Quiz

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,66 +2,54 @@ package app;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-        
-int count = 0;
-String color = "red";
-boolean colors = true; 
+  Scanner input = new Scanner(System.in);
+  System.out.println("what color is the sky");
+  String s1 = input.nextLine();
+int score =  0;
+  if (s1.equals("blue")) {
+      System.out.println("Right");
+score = score + 1;
+  } else {
+      System.out.println("wrong");
+  }
+  System.out.println("what color is dirt");
+  String s2 = input.nextLine();
+  if (s2.equals ("brown")) {
+      System.out.println("Right");
+      score = score + 1;
+  }
+  else {
+      System.out.println("wrong");
+  }
 
-while (count <= 10) {
-    System.out.println("The count is " + count);
-    count = count + 1;
-
+  System.out.println("what is the newest I phone");
+  String s3 = input.nextLine();
+  if (s3.equals("I phone 11 pro")) {
+      System.out.println("Right");
+      score = score + 1;
+  }
+  else {
+      System.out.println("wrong");
+  }
+  System.out.println("how many fingers on 2 hands");
+  String s4 = input.nextLine();
+  if (s4.equals("10")) {
+      System.out.println("Right");
+      score = score + 1;
+  }
+  else {
+      System.out.println("wrong");
+  }
+  System.out.println("who won the stanly cup");
+  String s5 = input.nextLine();
+  if (s5.equals("lightning")) {
+      System.out.println("Right");
+      score = score + 1;
+  }
+  else {
+      System.out.println("wrong");
+  }
+      
 }
-System.out.println("count finished");
-while (color == "red");{
-    System.out.println("The color is " + color);
-    color = "green";
-
-
-
-
-
-    
-
-    }
-System.out.println("count finished");
-    while (colors == true) {
-    System.out.println("The boolean is " + colors);
-    colors = false; }
-    }
-    
-}
-=======
-
-Scanner input = new Scanner(System.in);
-
-System.out.println("Please enter your first name.");
-String name = input.nextLine();
-System.out.println("Hello, " + name);
-
-System.out.println("Please emter your last name.");
-String lastname = input.nextLine();
-System.out.println("Hello, " + lastname);
-
-System.out.println("Please enter your age");
-String age = input.nextLine();
-System.out.println(age);
-
-System.out.println("Please enter your grade");
-String grade = input.nextLine();
-System.out.println(grade);
-
-System.out.println("Please enter your favorite color");
-String color = input.nextLine();
-System.out.println(color);
-
-
-
 }
 
-
-    
-
-}
->>>>>>> 14c28919ed73f2fcb4d8c2d22dabc335040bda3a
