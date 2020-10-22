afc41d908225d41d657dcba5bcb3ed83748e9c07
AJLester <827451@mystma.org>
2020-10-22 19:48:56 +0000

    chalenge 4

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,13 +1,32 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-int i = 0;
-while (i <= 1000) {
-  System.out.println("The number is" + i );
-  i = i + 1;
+Scanner input = new Scanner(System.in);
+
+System.out.println("Please enter a number");
+int num1 =input.nextInt();
+input.nextLine();
+
+System.out.println("Please enter a larger number");
+int num2 = input.nextInt();
+input.nextLine();
+
+num1++;
+
+while (num1 < num2){
+
+if (num1 % 2 == 0){
+    System.out.println(num1);
+
+    System.out.println(num1);
+}
+    num1++;
 
 }
-System.out.println();
+
+    }
 }
-}
+
